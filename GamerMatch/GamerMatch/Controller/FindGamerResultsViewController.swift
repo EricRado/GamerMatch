21d6c4b57daf7e22637ccb7dc024364dc54599a2
Eric Rado <erado@axxess.com>
2020-11-22 12:57:19 -0600

    Removed storyboard UI and replaced tableView with a collectionView in FindGamerResultsViewController.

--- GamerMatch/GamerMatch/Controller/FindGamerResultsViewController.swift
+++ GamerMatch/GamerMatch/Controller/FindGamerResultsViewController.swift
@@ -9,9 +9,8 @@
 import UIKit
 import Firebase
 
-class FindGamerResultsViewController: UIViewController {
-    
-    private let cellId = "gamerCell"
+final class FindGamerResultsViewController: UIViewController {
+
     private let vcIdentifier = "GamerProfileViewController"
     var results: [UserCacheInfo]?
     var resultIds: [String]?
@@ -29,25 +28,33 @@ class FindGamerResultsViewController: UIViewController {
         let manager = ImageManager(downloadSession: downloadSession)
         return manager
     }()
+
+	private lazy var collectionView: UICollectionView = {
+		let layout = UICollectionViewFlowLayout()
+		let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+		collectionView.translatesAutoresizingMaskIntoConstraints = false
+		collectionView.backgroundColor = .white
+		collectionView.register(UserSearchResultCell.self, forCellWithReuseIdentifier: UserSearchResultCell.identifier)
+		collectionView.dataSource = self
+		collectionView.delegate = self
+		return collectionView
+	}()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         tabBarController?.tabBar.isHidden = false
     }
-    
-    @IBOutlet weak var tableView: UITableView! {
-        didSet {
-            tableView.delegate = self
-            tableView.dataSource = self
-        }
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    }
   
     override func viewDidLoad() {
         super.viewDidLoad()
+		view.backgroundColor = .white
+		view.addSubview(collectionView)
+		NSLayoutConstraint.activate([
+			collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+			collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+		])
         
         results = [UserCacheInfo]()
         getUsersResults(from: resultIds)
@@ -72,8 +79,7 @@ class FindGamerResultsViewController: UIViewController {
                     guard let count = self.results?.count else { return }
                     let indexPath = IndexPath(row: count - 1, section: 0)
                     print("Inserting at row : \(indexPath.item)")
-					// CRASH
-                    self.tableView.insertRows(at: [indexPath], with: .none)
+					self.collectionView.insertItems(at: [indexPath])
                 }
             }
         }
@@ -81,48 +87,47 @@ class FindGamerResultsViewController: UIViewController {
 
 }
 
-extension FindGamerResultsViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return results?.count ?? 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! GamerMatchTableViewCell
-        guard let userCacheInfo = results?[indexPath.row] else { return cell }
-        
-        cell.gamerUsernameLabel.text = userCacheInfo.username
-        
-        if let urlString = userCacheInfo.url, urlString != "" {
-            let id = mediaManager.downloadImage(from: urlString)
-            guard let taskId = id else { return cell }
-            taskIdToCellRowDict[taskId] = indexPath.row
-            
-        } else {
-            cell.gamerAvatarImageView.image = UIImage(named: "noAvatarImg")
-        }
-        
-        return cell
-    }
+extension FindGamerResultsViewController: UICollectionViewDataSource {
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return results?.count ?? 0
+	}
+
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		guard let cell = collectionView
+				.dequeueReusableCell(withReuseIdentifier: UserSearchResultCell.identifier, for: indexPath)
+				as? UserSearchResultCell else { return UICollectionViewCell() }
+		guard let userCacheInfo = results?[indexPath.row] else { return cell }
+		
+		if let urlString = userCacheInfo.url, urlString != "" {
+			let id = mediaManager.downloadImage(from: urlString)
+			guard let taskId = id else { return cell }
+			taskIdToCellRowDict[taskId] = indexPath.row
+		}
+
+		return cell
+	}
 }
 
-extension FindGamerResultsViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let userCacheInfo = results?[indexPath.row] else { return }
-        
-        guard let vc = storyboard?
-            .instantiateViewController(withIdentifier: vcIdentifier)
-            as? GamerProfileViewController else { return }
-        vc.userCacheInfo = userCacheInfo
-        vc.userImage = cellRowToUserImage[indexPath.row]
-        tableView.deselectRow(at: indexPath, animated: false)
-        
-        navigationController?.navigationBar.topItem?.backBarButtonItem?.title = "Back"
-        navigationController?.pushViewController(vc, animated: false)
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return tableView.rowHeight
-    }
+extension FindGamerResultsViewController: UICollectionViewDelegateFlowLayout {
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+		collectionView.deselectItem(at: indexPath, animated: true)
+		guard let userCacheInfo = results?[indexPath.row] else { return }
+
+		guard let gamerProfileViewController = storyboard?
+			.instantiateViewController(withIdentifier: vcIdentifier)
+			as? GamerProfileViewController else { return }
+		gamerProfileViewController.userCacheInfo = userCacheInfo
+		gamerProfileViewController.userImage = cellRowToUserImage[indexPath.row]
+
+		navigationController?.navigationBar.topItem?.backBarButtonItem?.title = "Back"
+		navigationController?.pushViewController(gamerProfileViewController, animated: true)
+	}
+
+	func collectionView(
+		_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+		sizeForItemAt indexPath: IndexPath) -> CGSize {
+		return CGSize(width: collectionView.frame.width, height: 80)
+	}
 }
 
 extension FindGamerResultsViewController: URLSessionDownloadDelegate {
@@ -133,11 +138,14 @@ extension FindGamerResultsViewController: URLSessionDownloadDelegate {
         do {
             let data = try Data(contentsOf: location)
             DispatchQueue.main.async {
-                guard let row = self.taskIdToCellRowDict[taskId] else { return }
-                guard let cell = self.tableView.cellForRow(at: IndexPath(row: row, section: 0)) as? GamerMatchTableViewCell else { return }
+                guard let item = self.taskIdToCellRowDict[taskId] else { return }
+				guard let username = self.results?[item].username else { return }
+				let indexPath = IndexPath(item: item, section: 0)
+				guard let cell = self.collectionView.cellForItem(at: indexPath)
+						as? UserSearchResultCell else { return }
                 let image = UIImage(data: data)
-                cell.gamerAvatarImageView.image = image
-                self.cellRowToUserImage[row] = image
+				cell.configure(with: username, image: image)
+                self.cellRowToUserImage[item] = image
             }
         } catch let error {
             print(error)
@@ -153,19 +161,4 @@ extension FindGamerResultsViewController: URLSessionDownloadDelegate {
             }
         }
     }
-    
-    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
